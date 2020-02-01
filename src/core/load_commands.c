ecade7783647f451abf3870b0236ddd6a2ccc973
William Ta <wta@student.42.fr>
2020-02-01 17:02:58 +0100

    fix: add ptr range checker

--- src/core/load_commands.c
+++ src/core/load_commands.c
@@ -6,10 +6,11 @@
 /*   By: wta <wta@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/01 11:04:18 by wta               #+#    #+#             */
-/*   Updated: 2020/02/01 11:04:35 by wta              ###   ########.fr       */
+/*   Updated: 2020/02/01 11:48:49 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "nm.h"
 
 int		handle_load_commands(t_nm *nm)
@@ -22,6 +23,8 @@ int		handle_load_commands(t_nm *nm)
 	i = -1;
 	while (++i < nm->ncmds)
 	{
+		if (!ptr_valid_range(nm->content, nm->filestat.st_size, (void*)lc))
+			return (0);
 		if (lc->cmd == LC_SEGMENT || lc->cmd == LC_SEGMENT_64)
 			handle_sections(nm, (void*)lc);
 		else if (lc->cmd == LC_SYMTAB)
