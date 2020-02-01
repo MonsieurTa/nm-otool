5abf6cdb898f74de97755c44a8aefd41c716ca32
William Ta <wta@student.42.fr>
2020-02-01 11:44:02 +0100

    refactor: update format address function

--- src/core/format.c
+++ src/core/format.c
@@ -6,17 +6,19 @@
 /*   By: wta <wta@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/01 11:06:51 by wta               #+#    #+#             */
-/*   Updated: 2020/02/01 11:07:02 by wta              ###   ########.fr       */
+/*   Updated: 2020/02/01 11:26:11 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	format_symaddr(char dst[], uint64_t addr)
+void	format_symaddr(char c, char dst[], uint64_t addr)
 {
 	static const char	base[] = "0123456789abcdef";
 	uint32_t			i;
 
+	if (c == 'U')
+		return ((void)ft_strcpy(dst, "                "));
 	ft_strcpy(dst, "0000000000000000");
 	i = 16;
 	while (--i && addr)
--- src/core/list.c
+++ src/core/list.c
@@ -6,7 +6,7 @@
 /*   By: wta <wta@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/01 11:06:26 by wta               #+#    #+#             */
-/*   Updated: 2020/02/01 11:06:39 by wta              ###   ########.fr       */
+/*   Updated: 2020/02/01 11:27:20 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	push_result(t_nm *nm, uint64_t addr, uint8_t c, char *str)
 	t_list		*node;
 
 	ft_bzero(&node_content, sizeof(t_nm_result));
-	format_symaddr(node_content.symaddr, addr);
+	format_symaddr(c, node_content.symaddr, addr);
 	node_content.symchar = c;
 	node_content.symname = ft_strdup(str);
 	node = ft_lstnew(&node_content, sizeof(t_nm_result));
